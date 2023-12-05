42b9a271949a79d6876bc85586e69d637895e3c9
LubinaCH <149982839+LubinaCH@users.noreply.github.com>
2023-12-05 01:37:25 -0800

    Add files via upload

---
+++ biggestof3no.py
@@ -0,0 +1,15 @@
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+z=int(input("enter the third number:"))
+if(x>y):
+    if(x>z):
+        print(x,"is big")
+    else:
+        print(z,"is big")
+else:
+        if(y>z):
+            print(y,"is big")
+        else:
+            print(z,"is big")
+
+
