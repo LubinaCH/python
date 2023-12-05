8b33408f0863f88b43cb1e3d24346750cf7f08e4
LubinaCH <149982839+LubinaCH@users.noreply.github.com>
2023-12-05 01:49:36 -0800

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,16 @@
+rect=lambda l,b:l*b
+l=float(input("enter the length:"))
+b=float(input("enter the breadth:"))
+print("area of rectangle is:",rect(l,b))
+
+              
+square=lambda a:a*a
+a=float(input("enter the side:"))
+print("area of the square side",a,"is",square(a))
+
+
+traingle=lambda b,h:0.5*b*h
+b=float(input("enter the base:"))
+h=float(input("enter the height:"))
+print("area of traingle is:",traingle(b,h))
+
